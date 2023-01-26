45f0c3fdb5e014c4214868eaa7de5050010d23c4
Pop Maria <popmaria461@yahoo.com>
2023-01-26 17:45:11 +0200

    firebase setup

--- foodery/src/environments/environment.ts
+++ foodery/src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'foodery-c383c',
+    appId: '1:427518928461:web:49d1b9e71c24275f5e9e04',
+    databaseURL: 'https://foodery-c383c-default-rtdb.firebaseio.com',
+    storageBucket: 'foodery-c383c.appspot.com',
+    apiKey: 'AIzaSyAMAk-0_PZTeYgvGWx-Ly0_TQxuQF6iNoA',
+    authDomain: 'foodery-c383c.firebaseapp.com',
+    messagingSenderId: '427518928461',
+  },
   production: false
 };
 
