d685a8612c6fc0e683b200206a1a40acc6293bb2
Pop Maria <popmaria461@yahoo.com>
2023-01-29 20:53:30 +0200

    finished signup and login

--- foodery/src/app/app.module.ts
+++ foodery/src/app/app.module.ts
@@ -3,21 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire/compat';
+import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    SignupPageComponent,
+    LoadingSpinnerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth()),
-    provideDatabase(() => getDatabase())
+    FormsModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- foodery/src/app/pages/signup-page/signup-page.component.ts
+++ foodery/src/app/pages/signup-page/signup-page.component.ts
@@ -1,15 +1,69 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-signup-page',
   templateUrl: './signup-page.component.html',
-  styleUrls: ['./signup-page.component.scss']
+  styleUrls: ['./signup-page.component.scss'],
 })
 export class SignupPageComponent implements OnInit {
+  public isLoading = false;
+  public displayValidationMessage = false;
+  public passValidationMessage = false;
+  public submitted = false;
+  signupForm!: FormGroup;
+  firebaseErrorMessage: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private db: AngularFireDatabase
+  ) {
+    this.firebaseErrorMessage = '';
   }
 
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.pattern('^(?=[^A-Z]*[A-Z])(?=[^a-z]*[a-z])(?=\\D*\\d)[A-Za-z\\d!$%@#£€*?&]{6,}$')]),
+    });
+  }
+
+  onSubmit() {
+    if (this.signupForm.invalid) {
+      this.displayValidationMessage = true;
+      return;
+    }
+    this.displayValidationMessage = false;
+    this.isLoading = true;
+    var formData = {
+      name: this.signupForm.value.name,
+    };
+
+    this.authService
+      .signupUser(this.signupForm.value)
+      .then(async (result) => {
+        if (result == null) {
+          let username = this.signupForm.value.email.replace(/[^a-z0-9]/gi, '')
+          await this.db.object('users/' + username + '/info').set(formData);
+          localStorage.setItem("user", username);
+
+          this.router.navigate(['../login']);
+        }
+        else {
+          this.isLoading = false;
+        }
+      })
+      .catch(() => {
+        alert('Error');
+      });
+  }
+
+  switchToLogin() {
+    this.router.navigate(['./login']);
+  }
 }
